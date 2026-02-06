23da94153720ea01704976c85ac39a16b691906b
Miguel Duarte <fmiguelduarte@gmail.com>
2026-02-06 00:44:32 +0000

    feat: Add new dynamic Matchers: ALL_REQUIRED, DISABLED_AND_HIDDEN and CLEAN_VALUE

--- projects/ng-dynamic-forms/core/src/lib/service/dynamic-form-relation-matchers.ts
+++ projects/ng-dynamic-forms/core/src/lib/service/dynamic-form-relation-matchers.ts
@@ -1,10 +1,11 @@
 import { InjectionToken, Injector, ValueProvider } from '@angular/core';
-import { UntypedFormControl } from '@angular/forms';
+import { FormControl, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 import { DynamicFormControlModel } from '../model/dynamic-form-control.model';
 import { DynamicFormValidationService } from './dynamic-form-validation.service';
 import { isObject } from '../utils/core.utils';
 import { DynamicValidatorsConfig } from '../model/misc/dynamic-form-control-validation.model';
 import { DynamicFormService } from './dynamic-form.service';
+import { DynamicCheckboxGroupModel, DynamicFormArrayGroupModel, DynamicFormGroupModel, DynamicFormModel, DynamicFormValueControlModel } from '../core';
 
 export const MATCH_DISABLED = 'DISABLED';
 export const MATCH_ENABLED = 'ENABLED';
@@ -12,6 +13,12 @@ export const MATCH_HIDDEN = 'HIDDEN';
 export const MATCH_OPTIONAL = 'OPTIONAL';
 export const MATCH_REQUIRED = 'REQUIRED';
 export const MATCH_VISIBLE = 'VISIBLE';
+export const MATCH_ALL_REQUIRED = 'ALL_REQUIRED';
+export const MATCH_NONE_REQUIRED = 'NONE_REQUIRED';
+export const MATCH_DISABLED_AND_HIDDEN = 'DISABLED_AND_HIDDEN';
+export const MATCH_ENABLE_AND_VISIBLE = 'ENABLE_AND_VISIBLE';
+export const MATCH_CLEAN_VALUE = 'CLEAN_VALUE';
+export const MATCH_DONT_CLEAN_VALUE = 'DONT_CLEAN_VALUE';
 
 export const AND_OPERATOR = 'AND';
 export const OR_OPERATOR = 'OR';
@@ -62,6 +69,78 @@ export const REQUIRED_MATCHER: DynamicFormControlMatcher = {
     }
 };
 
+export const ALL_REQUIRED_MATCHER: DynamicFormControlMatcher = {
+    match: MATCH_ALL_REQUIRED,
+    opposingMatch: MATCH_NONE_REQUIRED,
+  onChange(hasMatch: boolean, model: DynamicFormControlModel, control: FormControl, injector: Injector): void {
+    const requireGroup = (group: DynamicFormModel, required: boolean, controls: { [key: string]: UntypedFormControl }) => {
+      group
+        .filter((innerModel) => !innerModel.hidden)
+        .forEach((innerModel: DynamicFormControlModel) => {
+          let innerControl = controls[innerModel.id];
+          if (innerModel instanceof DynamicFormValueControlModel) {
+            REQUIRED_MATCHER.onChange(hasMatch, innerModel, innerControl, injector);
+          } else if (
+            innerModel instanceof DynamicFormGroupModel ||
+            innerModel instanceof DynamicCheckboxGroupModel ||
+            innerModel instanceof DynamicFormArrayGroupModel
+          ) {
+            requireGroup(innerModel.group, required, (innerControl as any)['controls']);
+          }
+        });
+    };
+    if (model instanceof DynamicFormGroupModel && !model.hidden) {
+      requireGroup(model.group, hasMatch, (control as any)['controls']);
+    } else if (model instanceof DynamicFormValueControlModel && model.required !== hasMatch) {
+      REQUIRED_MATCHER.onChange(hasMatch, model, control, injector);
+    }
+  }
+};
+
+export const ENABLE_AND_VISIBLE_MATCHER: DynamicFormControlMatcher = {
+  match: MATCH_DISABLED_AND_HIDDEN,
+  opposingMatch: MATCH_ENABLE_AND_VISIBLE,
+  onChange(hasMatch: boolean, model: DynamicFormControlModel, control: FormControl, injector: Injector): void {
+    if (model instanceof DynamicFormValueControlModel) {
+      model.value = hasMatch ? undefined : model.value;
+    }
+    model.disabled = hasMatch;
+    model.hidden = hasMatch;
+  }
+};
+
+export const CLEAN_VALUE_MATCHER: DynamicFormControlMatcher = {
+  match: MATCH_CLEAN_VALUE,
+  opposingMatch: MATCH_DONT_CLEAN_VALUE,
+  onChange(hasMatch: boolean, model: DynamicFormControlModel, control: FormControl): void {
+    const cleanGroup = (group: DynamicFormModel, required: boolean, controls: { [key: string]: UntypedFormControl }) => {
+      group
+        .filter((innerModel) => !innerModel.hidden)
+        .forEach((innerModel: DynamicFormControlModel) => {
+          let innerControl = controls[innerModel.id];
+          if (innerModel instanceof DynamicFormValueControlModel) {
+            if (hasMatch) {
+              innerControl.reset();
+            }
+          } else if (
+            innerModel instanceof DynamicFormGroupModel ||
+            innerModel instanceof DynamicCheckboxGroupModel ||
+            innerModel instanceof DynamicFormArrayGroupModel
+          ) {
+            cleanGroup(innerModel.group, required, innerControl as any['controls']);
+          }
+        });
+    };
+    if (model instanceof DynamicFormGroupModel && !model.hidden) {
+      cleanGroup(model.group, hasMatch, control as any['controls']);
+    } else if (model instanceof DynamicFormValueControlModel && model.required !== hasMatch) {
+      if (hasMatch) {
+        control.reset();
+      }
+    }
+  }
+};
+
 export const DISABLED_MATCHER_PROVIDER: ValueProvider = {
     provide: DYNAMIC_MATCHERS,
     useValue: DISABLED_MATCHER,
@@ -80,4 +159,22 @@ export const REQUIRED_MATCHER_PROVIDER: ValueProvider = {
     multi: true
 };
 
-export const DYNAMIC_MATCHER_PROVIDERS = [DISABLED_MATCHER_PROVIDER, HIDDEN_MATCHER_PROVIDER, REQUIRED_MATCHER_PROVIDER];
+export const ALL_REQUIRED_MATCHER_PROVIDER: ValueProvider = {
+    provide: DYNAMIC_MATCHERS,
+    useValue: ALL_REQUIRED_MATCHER,
+    multi: true
+};
+
+export const ENABLE_AND_VISIBLE_MATCHER_PROVIDER: ValueProvider = {
+  provide: DYNAMIC_MATCHERS,
+  useValue: ENABLE_AND_VISIBLE_MATCHER,
+  multi: true
+};
+
+export const CLEAN_VALUE_MATCHER_PROVIDER: ValueProvider = {
+  provide: DYNAMIC_MATCHERS,
+  useValue: CLEAN_VALUE_MATCHER,
+  multi: true
+};
+
+export const DYNAMIC_MATCHER_PROVIDERS = [DISABLED_MATCHER_PROVIDER, HIDDEN_MATCHER_PROVIDER, REQUIRED_MATCHER_PROVIDER, ALL_REQUIRED_MATCHER_PROVIDER, ENABLE_AND_VISIBLE_MATCHER_PROVIDER, CLEAN_VALUE_MATCHER_PROVIDER];
